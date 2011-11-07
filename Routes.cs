1bd37287daf541040d0ec42e05cedf6a2391db93
Jeremiah Redekop <jeremiah@geniuscode.net>
2011-11-07 14:21:17 -0800

    initial dump

---
+++ Routes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Activation;
+using System.Text;
+using System.Web.Routing;
+using Microsoft.ApplicationServer.Http;
+using Microsoft.ApplicationServer.Http.Activation;
+using Orchard.Mvc.Routes;
+using Orchard.Wcf;
+using myRestfulModule.Services;
+
+namespace myRestfulModule
+{
+    public class Routes : IRouteProvider {
+        private static readonly ServiceRoute _route = MapServiceRoute(typeof (PeopleService), "myRestfulModule/People");
+
+        public IEnumerable<RouteDescriptor> GetRoutes() {
+        
+            
+            
+            return new[] {
+                new RouteDescriptor {
+                    Priority = 1,
+                    Route = _route
+                }
+
+
+
+                
+
+            };
+
+            
+        }
+
+        public void GetRoutes(ICollection<RouteDescriptor> routes) {
+            foreach (var routeDescriptor in GetRoutes())
+                routes.Add(routeDescriptor);
+        }
+
+        private static ServiceRoute MapServiceRoute(Type serviceType, string routePrefix, HttpConfiguration configuration = null, object constraints = null, bool useMethodPrefixForHttpMethod = true)
+        {
+            if (configuration == null)
+                configuration = new WebApiConfiguration(useMethodPrefixForHttpMethod);
+
+            var serviceHostFactory = new HttpServiceHostFactory { Configuration = configuration };
+            var webApiRoute = new WebApiRoute(routePrefix, serviceHostFactory, serviceType) { Constraints = new RouteValueDictionary(constraints) };
+            return webApiRoute;
+        }
+    }
+}
---
+++ Services/PeopleService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Orchard;
+using myRestfulModule.Models;
+
+namespace myRestfulModule.Services
+{
+    [ServiceContract]
+    public class PeopleService 
+        : IDependency // required for orchard
+    {
+        private static readonly List<Person> _people = Fabrication.Fabricator.Generate<Person>(500).ToList();
+
+        [WebGet]
+        public IQueryable<Person> GetPeople()
+        {
+            
+            return _people.AsQueryable();
+            //return new EnumerableQuery<Person>(new Person[]{});
+        }
+
+        [WebGet(UriTemplate = "{name}")]
+        public Person GetPerson(string name) {
+            return _people.Single(p=> p.Name == name);
+        }
+    }
+}
